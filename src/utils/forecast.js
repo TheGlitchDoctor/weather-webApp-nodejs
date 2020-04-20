8e53ce7b60cb747e8c9cfe96ea35c55e66677b98
Tushar <tabana@mitaoe.ac.in>
2020-04-20 20:49:47 +0530

    Fahrenheit to Celsius Temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request')
 
 
 const forecast = (latitude, longitude, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=07a2ac93a2f177e98f83baee545eed1c&query=" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "&units=f";
+    const url = "http://api.weatherstack.com/current?access_key=07a2ac93a2f177e98f83baee545eed1c&query=" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "&units=c";
 
     request({
         url,
@@ -15,7 +15,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to fetch weather for location. Check query parameters again!', undefined)
         } else {
             
-            callback(undefined, body.current.weather_descriptions[0] + " : It is currently " + body.current.temperature + " degrees fahrenheit outside. It feels like " + body.current.feelslike + " degrees fahrenheit.\nObservation Time -> " + body.current.observation_time + "\n. Humidity is " + body.current.humidity + " percent.\nVisibility -> " + body.current.visibility)
+            callback(undefined, body.current.weather_descriptions[0] + " : It is currently " + body.current.temperature + " degree Celsius outside. It feels like " + body.current.feelslike + " degrees Celsius.\nObservation Time -> " + body.current.observation_time + "\n. Humidity is " + body.current.humidity + " percent.\nVisibility -> " + body.current.visibility)
         }
     })
 }
